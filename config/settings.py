1bc75650eb1a2db3fb81e85d7b2cfba061db09ca
Tiffany Subin Kim <sbtiffanykim@gmail.com>
2025-07-23 16:27:13 +0900

    feat: add DRF and organize installed apps

--- config/settings.py
+++ config/settings.py
@@ -29,6 +29,9 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
+THIRD_PARTY_APPS = [
+    "rest_framework",
+]
 
 CUSTOM_APPS = [
     "users.apps.UsersConfig",
@@ -49,7 +52,7 @@ SYSTEM_APPS = [
     "django.contrib.staticfiles",
 ]
 
-INSTALLED_APPS = CUSTOM_APPS + SYSTEM_APPS
+INSTALLED_APPS = SYSTEM_APPS + CUSTOM_APPS + THIRD_PARTY_APPS
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
