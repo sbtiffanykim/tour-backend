e9314c194fc2332baaa33af14fd7a603f4973dc6
Tiffany Subin Kim <sbtiffanykim@gmail.com>
2025-07-23 16:50:11 +0900

    feat: implement custom User model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 
-# Create your models here.
+
+class User(AbstractUser):
+
+    phone_validator = RegexValidator(regex=r"^01[0-9]-\d{3,4}-\d{4}$", message="The phone number format is invalid")
+
+    avatar = models.ImageField()
+    phone_number = models.CharField(max_length=15, validators=[phone_validator])
+    points = models.PositiveIntegerField(default=0)
