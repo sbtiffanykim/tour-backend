9dbd1b033f299e57273e7c258de47c8fb28bb87c
Tiffany Subin Kim <sbtiffanykim@gmail.com>
2025-07-24 16:28:32 +0900

    feat: implement user admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+
+    list_display = ["username", "is_staff", "is_superuser"]
+    list_filter = ["is_staff", "is_superuser"]
+
+    fieldsets = (
+        (("Personal info"), {"fields": ["username", "email", "first_name", "last_name", "phone_number"]}),
+        (
+            ("Permissions"),
+            {
+                "fields": ["is_active", "is_staff", "is_superuser"],
+                "classes": ["collapse"],
+            },
+        ),
+        (("Additional info"), {"fields": ["avatar", "points", "last_login", "date_joined"], "classes": ["collapse"]}),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ["wide"],
+                "fields": ["email", "password1", "password2", "phone_number", "first_name", "last_name"],
+            },
+        ),
+    )
+
+    search_fields = ["username", "phone_number"]
+    ordering = ["username"]
