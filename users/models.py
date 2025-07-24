3da1d54c8ce7780cbfb1d23e9798ec5cc765a4f7
Tiffany Subin Kim <sbtiffanykim@gmail.com>
2025-07-24 17:11:09 +0900

    feat: make first_name, last_name, email required

--- users/models.py
+++ users/models.py
@@ -1,12 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import RegexValidator
+from django.core.validators import EmailValidator
 
 
 class User(AbstractUser):
 
     phone_validator = RegexValidator(regex=r"^01[0-9]-\d{3,4}-\d{4}$", message="The phone number format is invalid")
 
-    avatar = models.ImageField()
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(
+        unique=True,
+        validators=[EmailValidator],
+        error_messages={
+            "unique": ("A user with that email already exists."),
+        },
+    )
+    avatar = models.ImageField(null=True, blank=True)
     phone_number = models.CharField(max_length=15, validators=[phone_validator])
     points = models.PositiveIntegerField(default=0)
