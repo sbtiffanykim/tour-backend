366e4592110b8918373b65a060fa10537d5ef335
Tiffany Subin Kim <sbtiffanykim@gmail.com>
2025-07-24 17:27:38 +0900

    chore(config): fix typos in urls

--- config/urls.py
+++ config/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("users/", include("users.urls")),
+    path("api/v1/users/", include("users.urls")),
 ]
