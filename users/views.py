1ecffe6b77134737b2e90f1b13d3f0a03ceee56c
Tiffany Subin Kim <sbtiffanykim@gmail.com>
2025-07-25 15:16:22 +0900

    feat(user): implement sign up api view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import SignUpSeriailzer
 
-# Create your views here.
+
+class SignUpView(APIView):
+
+    def post(self, request):
+        serializer = SignUpSeriailzer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                {
+                    "message": "User created successfully",
+                    "user": {
+                        "Username": serializer.data["username"],
+                        "First name": serializer.data["first_name"],
+                        "Last name": serializer.data["last_name"],
+                        "Email": serializer.data["email"],
+                        "Phone number": serializer.data["phone_number"],
+                    },
+                },
+                status=status.HTTP_201_CREATED,
+            )
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
