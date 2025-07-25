076d2aa5f26356ce6b8f0f57d98c579ed0fbf7b7
Tiffany Subin Kim <sbtiffanykim@gmail.com>
2025-07-26 00:44:35 +0900

    refactor(users): change PrivateUserView URL from /users/profile to /users/me

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path("sign-up", views.SignUpView.as_view()),
     path("logout", views.LogOutView.as_view()),
     path("login", views.LogInView.as_view()),
-    path("profile", views.PrivateUserView.as_view()),
+    path("me", views.PrivateUserView.as_view()),
 ]
