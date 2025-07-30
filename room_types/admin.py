9a81efdcc05db36e2b7ba3b081252f7fe8de2811
Tiffany Subin Kim <sbtiffanykim@gmail.com>
2025-07-30 23:05:08 +0900

    feat(room_types): add list_filter and search_fields to admin

--- room_types/admin.py
+++ room_types/admin.py
@@ -14,9 +14,13 @@ class RoomTypeAdmin(admin.ModelAdmin):
         (("Info"), {"fields": ["accommodation", "name", "base_occupancy", "max_occupancy", "description"]}),
         (("Number of rooms"), {"fields": ["num_living_room", "num_bedrooms", "num_bathrooms"]}),
     ]
+    list_filter = ["accommodation__name"]
+    search_fields = ["name", "accommodation__name"]
     inlines = [BedConfigurationInline]
 
 
 @admin.register(BedConfiguration)
 class BedConfigureAdmin(admin.ModelAdmin):
     list_display = ["room_type", "bed_type", "count"]
+    list_filter = ["room_type__accommodation__name"]
+    search_fields = ["room_type__name", "room_type__accommodation__name"]
