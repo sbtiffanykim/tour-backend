b30f12f40c511d8b85d2ac627d4cd093d0858ee9
Tiffany Subin Kim <sbtiffanykim@gmail.com>
2025-07-30 16:06:11 +0900

    feat(accommodations): add region chioce in the model

--- accommodations/admin.py
+++ accommodations/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Accommodation, Amenity
 
-# Register your models here.
+
+@admin.register(Accommodation)
+class AccommodationAdmin(admin.ModelAdmin):
+    list_display = ("name", "region")
+    list_filter = ("region",)
+    search_fields = ("name",)
+
+
+@admin.register(Amenity)
+class AmenityAdmin(admin.ModelAdmin):
+    pass
--- accommodations/models.py
+++ accommodations/models.py
@@ -1,11 +1,22 @@
 from django.db import models
 
 
+class RegionChioce(models.TextChoices):
+    SEOUL = "seoul", "Seoul"
+    GYEONGGI = "gyeonggi", "Gyeonggi"
+    GANGWON = "gangwon", "Gangwon"
+    CHUNGCHEONG = "chungcheong", "Chungcheong"
+    GYEONGSANG = "gyeongsang", "Gyeongsang"
+    JEOLLA = "jeolla", "Jeolla"
+    JEJU = "jeju", "Jeju"
+
+
 class Accommodation(models.Model):
     """Model Definition for accomodations"""
 
     name = models.CharField(max_length=50)
     location = models.CharField(max_length=150)
+    region = models.CharField(max_length=20, choices=RegionChioce)
     x_coordinate = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
     y_coordinate = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
     homepage = models.CharField(max_length=100, null=True, blank=True)
@@ -14,7 +25,10 @@ class Accommodation(models.Model):
     check_out = models.TimeField(null=True, blank=True)
     cancellation_policy = models.TextField(null=True, blank=True)
     info = models.TextField(null=True, blank=True)
-    amenities = models.ManyToManyField("accommodations.Amenity", related_name="accomodations")
+    amenities = models.ManyToManyField("accommodations.Amenity", related_name="accomodations", null=True, blank=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Amenity(models.Model):
