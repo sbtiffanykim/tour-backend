cd14083cdf630752b4f6b8072901f9359d6055d2
Tiffany Subin Kim <sbtiffanykim@gmail.com>
2025-07-31 16:46:18 +0900

    feat(room_types): add serializer

---
+++ room_types/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework.serializers import ModelSerializer
+from .models import RoomType
+from packages.serializers import PackageSerializer
+
+
+class RoomTypeWithPackageSerializer(ModelSerializer):
+
+    packages = PackageSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = RoomType
+        fields = ("pk", "name", "packages")
