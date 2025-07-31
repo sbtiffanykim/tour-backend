164cdc7fbbf31628a7afdb9fbd7d6d83d78a4fad
Tiffany Subin Kim <sbtiffanykim@gmail.com>
2025-07-31 16:55:21 +0900

    feat(packages): add PackagePrice model to implement daily prices

--- packages/models.py
+++ packages/models.py
@@ -12,3 +12,14 @@ class Package(models.Model):
 
     def __str__(self):
         return f"{self.room_type} - {self.name}"
+
+
+class PackagePrice(models.Model):
+    """Model Definition for daily package price"""
+
+    package = models.ForeignKey("packages.Package", on_delete=models.CASCADE, related_name="daily_price")
+    date = models.DateField()
+    price = models.PositiveIntegerField(help_text="daily price")
+
+    class Meta:
+        unique_together = ("package", "date")
