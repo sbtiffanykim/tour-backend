3b38d5840a6a1c0bb2796f4508d2fbd790dcf1a8
Tiffany Subin Kim <sbtiffanykim@gmail.com>
2025-08-07 14:37:00 +0900

    feat(wishlists): add WishlistAdmin

--- wishlists/admin.py
+++ wishlists/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Wishlist
 
-# Register your models here.
+
+@admin.register(Wishlist)
+class WishlistAdmin(admin.ModelAdmin):
+    list_display = ["__str__", "user", "num_of_accommodations"]
+    list_filter = ["user", "accommodations"]
+    search_fields = ["accommodations__name", "user__username"]
