4057e2f4cef565e0744a0def1fbdeaad09d2d436
Tiffany Subin Kim <sbtiffanykim@gmail.com>
2025-08-07 14:34:56 +0900

    feat(wishlists): implement Wishlist model and migrate

--- wishlists/models.py
+++ wishlists/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from users.models import User
+from accommodations.models import Accommodation
 
-# Create your models here.
+
+class Wishlist(models.Model):
+    name = models.CharField(max_length=40)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="wishlists")
+    accommodations = models.ManyToManyField(Accommodation, related_name="wishlists", blank=True)
+
+    def num_of_accommodations(self):
+        return self.accommodations.count()
+
+    def __str__(self):
+        return self.name
