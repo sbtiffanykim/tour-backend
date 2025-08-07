83fef862ca66574e7e0ef65cf9eb90a58c0a8b00
Tiffany Subin Kim <sbtiffanykim@gmail.com>
2025-08-07 21:00:24 +0900

    test(wishlists): add success & failure cases for 2 accommodation-related views

--- wishlists/test_wishlists.py
+++ wishlists/test_wishlists.py
@@ -8,6 +8,12 @@ from accommodations.models import Accommodation, City
 # ----- Constraints ----
 WISHLIST_DETAIL_URL = lambda pk: f"/api/v1/wishlists/{pk}"
 CREATE_WISHLIST_URL = f"/api/v1/wishlists/create"
+ADD_ACC_TO_WISHLIST_URL = (
+    lambda wishlist_pk, accommodation_pk: f"/api/v1/wishlists/{wishlist_pk}/add/{accommodation_pk}"
+)
+REMOVE_ACC_FROM_WISHLIST_URL = (
+    lambda wishlist_pk, accommodation_pk: f"/api/v1/wishlists/{wishlist_pk}/remove/{accommodation_pk}"
+)
 
 # ----- Fixtures -----
 
@@ -46,6 +52,12 @@ def authenticated_client(client, sample_db):
     user = sample_db["user"]
     client.force_login(user)
     return client
+
+
+@pytest.fixture()
+def new_accommodation():
+    city = City.objects.create(name="Busan")
+    return Accommodation.objects.create(name="Resort A", region="busan", location="test", city=city, type="resort")
 
 
 # ----- WishlistDetailView Test -----
@@ -142,3 +154,105 @@ def test_create_wishlist_already_exist(authenticated_client, sample_db):
 
     assert response.status_code == 400
     assert response.data["error"] == "You already have a wishlist"
+
+
+# ----- AddAccommodationToWishlistView Test -----
+
+
+# Success
+@pytest.mark.django_db
+def test_add_acc_to_wishlist_success(authenticated_client, sample_db, new_accommodation):
+    initial_num = sample_db["wishlist"].accommodations.count()
+    new_accommodation = new_accommodation
+    wishlist = sample_db["wishlist"]
+    response = authenticated_client.post(ADD_ACC_TO_WISHLIST_URL(wishlist.pk, new_accommodation.pk))
+
+    assert response.status_code == 200
+    assert response.data["success"] == "Accommodation added"
+    wishlist.refresh_from_db()
+    assert wishlist.accommodations.count() == initial_num + 1
+
+
+# Failure 1: Wishlist does not exist
+@pytest.mark.django_db
+def test_add_acc_to_wishlist_wishlist_not_found(authenticated_client, new_accommodation):
+    new_accommodation = new_accommodation
+    non_existent_wishlist_pk = 10000  # assume wishlist with this pk does not exist
+    response = authenticated_client.post(ADD_ACC_TO_WISHLIST_URL(non_existent_wishlist_pk, new_accommodation.pk))
+    assert response.status_code == 404
+    assert response.data["error"] == "Wishlist not found"
+
+
+# Failure 2: No permission
+@pytest.mark.django_db
+def test_add_acc_to_wishlist_no_permission(client, sample_db, new_accommodation, user_payload):
+    wishlist = sample_db["wishlist"]
+    user_payload.update({"username": "test2", "email": "test2@gmail.com"})
+    new_user = User.objects.create(**user_payload)
+    client.force_login(new_user)
+    new_accommodation = new_accommodation
+    response = client.post(ADD_ACC_TO_WISHLIST_URL(wishlist.pk, new_accommodation.pk))
+
+    assert response.status_code == 403
+    assert response.data["error"] == "You do not have a permission to access this wishlist"
+
+
+# Failure 3: Accommodation does not exist
+@pytest.mark.django_db
+def test_add_acc_to_wishlist_acc_not_found(authenticated_client, sample_db):
+    wishlist = sample_db["wishlist"]
+    non_existent_accommodation_pk = 10000  # assume accommodation with this pk does not exist
+    response = authenticated_client.post(ADD_ACC_TO_WISHLIST_URL(wishlist.pk, non_existent_accommodation_pk))
+    assert response.status_code == 404
+    assert response.data["detail"] == "Accommodation not found"
+
+
+# ----- RemoveAccommodationToWishlistView Test -----
+
+
+# Success
+@pytest.mark.django_db
+def test_remove_acc_from_wishlist_success(authenticated_client, sample_db):
+    initial_num = sample_db["wishlist"].accommodations.count()
+    accommodation = sample_db["accommodation"]
+    wishlist = sample_db["wishlist"]
+    response = authenticated_client.delete(REMOVE_ACC_FROM_WISHLIST_URL(wishlist.pk, accommodation.pk))
+
+    assert response.status_code == 200
+    assert response.data["success"] == "Accommodation removed"
+    wishlist.refresh_from_db()
+    assert wishlist.accommodations.count() == initial_num - 1
+
+
+# Failure 1: Wishlist does not exist
+@pytest.mark.django_db
+def test_remove_acc_from_wishlist_wishlist_not_found(authenticated_client, sample_db):
+    accommodation = sample_db["accommodation"]
+    non_existent_wishlist_pk = 10000  # assume wishlist with this pk does not exist
+    response = authenticated_client.delete(REMOVE_ACC_FROM_WISHLIST_URL(non_existent_wishlist_pk, accommodation.pk))
+    assert response.status_code == 404
+    assert response.data["error"] == "Wishlist not found"
+
+
+# Failure 2: No permission
+@pytest.mark.django_db
+def test_remove_acc_from_wishlist_no_permission(client, sample_db, user_payload):
+    wishlist = sample_db["wishlist"]
+    user_payload.update({"username": "test2", "email": "test2@gmail.com"})
+    new_user = User.objects.create(**user_payload)
+    client.force_login(new_user)
+    accommodation = sample_db["accommodation"]
+    response = client.delete(REMOVE_ACC_FROM_WISHLIST_URL(wishlist.pk, accommodation.pk))
+
+    assert response.status_code == 403
+    assert response.data["error"] == "You do not have a permission to access this wishlist"
+
+
+# Failure 3: Accommodation does not exist
+@pytest.mark.django_db
+def test_remove_acc_from_wishlist_acc_not_found(authenticated_client, sample_db):
+    wishlist = sample_db["wishlist"]
+    non_existent_accommodation_pk = 10000  # assume accommodation with this pk does not exist
+    response = authenticated_client.delete(REMOVE_ACC_FROM_WISHLIST_URL(wishlist.pk, non_existent_accommodation_pk))
+    assert response.status_code == 404
+    assert response.data["detail"] == "Accommodation not found"
