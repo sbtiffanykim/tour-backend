553bf3c92028352d6b49e7b840881764cf7b9e04
Tiffany Subin Kim <sbtiffanykim@gmail.com>
2025-08-07 19:06:58 +0900

    feat(wishlists): add Add&remove accommodation to wishlist view and route

--- wishlists/urls.py
+++ wishlists/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path("create", views.CreateWishlistView.as_view()),
     path("<int:pk>", views.WishlistDetailView.as_view()),
+    path("<int:pk>/add/<int:accommodation_pk>", views.AddAccommodationToWishlistView.as_view()),
+    path("<int:pk>/remove/<int:accommodation_pk>", views.RemoveAccommodationToWishlistView.as_view()),
 ]
--- wishlists/views.py
+++ wishlists/views.py
@@ -6,6 +6,7 @@ from rest_framework import status
 
 from .models import Wishlist
 from .serializers import WishlistDetailSerializer
+from accommodations.models import Accommodation
 
 
 def get_wishlist_or_404(pk, user):
@@ -56,3 +57,31 @@ class CreateWishlistView(APIView):
         wishlist = Wishlist.objects.create(name=name, user=request.user)
         serializer = WishlistDetailSerializer(wishlist)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class AddAccommodationToWishlistView(APIView):
+    """API view to add an accommodation to the specified wishlist if both exist and the user has permission"""
+
+    def post(self, request, pk, accommodation_pk):
+        wishlist = get_wishlist_or_404(pk, request.user)
+        try:
+            accommodation = Accommodation.objects.get(pk=accommodation_pk)
+        except Accommodation.DoesNotExist:
+            raise NotFound("Accommodation not found")
+        wishlist.accommodations.add(accommodation)
+        serializer = WishlistDetailSerializer(wishlist)
+        return Response({"success": "Accommodation added", "wishlist": serializer.data}, status=status.HTTP_200_OK)
+
+
+class RemoveAccommodationToWishlistView(APIView):
+    """API view to remove an accommodation to the specified wishlist if both exist and the user has permission"""
+
+    def delete(self, request, pk, accommodation_pk):
+        wishlist = get_wishlist_or_404(pk, request.user)
+        try:
+            accommodation = Accommodation.objects.get(pk=accommodation_pk)
+        except Accommodation.DoesNotExist:
+            raise NotFound("Accommodation not found")
+        wishlist.accommodations.remove(accommodation)
+        serializer = WishlistDetailSerializer(wishlist)
+        return Response({"success": "Accommodation removed", "wishlist": serializer.data}, status=status.HTTP_200_OK)
