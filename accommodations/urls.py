2c23827ec7318d80b7c44b5c615ca843d8c975d5
Tiffany Subin Kim <sbtiffanykim@gmail.com>
2025-09-26 15:33:25 +0900

    fix(accommodations): update urls to use moved views in packages

--- accommodations/urls.py
+++ accommodations/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
+from packages.views import RoomPackageListView, AvailableRoomPackagesView
 
 urlpatterns = [
     path("", views.AccommodationListView.as_view()),
     path("<int:pk>", views.AccommodationDetailView.as_view()),
-    path("<int:pk>/room-packages", views.RoomPackageListView.as_view()),
-    path("<int:pk>/available-room-packages", views.AvailableRoomPackagesView.as_view()),
+    path("<int:pk>/room-packages", RoomPackageListView.as_view()),
+    path("<int:pk>/available-room-packages", AvailableRoomPackagesView.as_view()),
 ]
