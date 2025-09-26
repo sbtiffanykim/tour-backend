0bca4e21db87299bd893f1ff65b73fd98a819dd0
Tiffany Subin Kim <sbtiffanykim@gmail.com>
2025-09-26 23:49:56 +0900

    feat(users): add CustomBaseUser and GuestInfo models

--- users/models.py
+++ users/models.py
@@ -4,7 +4,8 @@ from django.core.validators import RegexValidator
 from django.core.validators import EmailValidator
 
 
-class User(AbstractUser):
+class CustomBaseUser(models.Model):
+    """Abstract base model for all users(registered or non-registered). Stores common information."""
 
     phone_validator = RegexValidator(regex=r"^01[0-9]{8,9}$", message="The phone number format is invalid")
 
@@ -17,6 +18,21 @@ class User(AbstractUser):
             "unique": ("A user with that email already exists."),
         },
     )
-    avatar = models.ImageField(null=True, blank=True)
     phone_number = models.CharField(max_length=15, validators=[phone_validator])
+
+    class Meta:
+        abstract = True
+
+
+class User(AbstractUser, CustomBaseUser):
+    """Custom user model for registered users."""
+
+    avatar = models.ImageField(null=True, blank=True)
     points = models.PositiveIntegerField(default=0)
+
+
+class GuestInfo(CustomBaseUser):
+    """Stores guest information for non-register bookings."""
+
+    def __str__(self):
+        return f"Guest: {self.first_name} {self.last_name}_{self.phone_number}"
