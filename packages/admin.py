04d3cdac84ce6f966169c1429ced95f9a38a640b
Tiffany Subin Kim <sbtiffanykim@gmail.com>
2025-09-26 15:34:04 +0900

    fix(packages): add fields in admins' list_display

--- packages/admin.py
+++ packages/admin.py
@@ -4,12 +4,12 @@ from .models import Package, PackageDailyAvailability
 
 @admin.register(Package)
 class PackageAdmin(admin.ModelAdmin):
-    list_display = ["__str__", "price"]
+    list_display = ["__str__", "base_price"]
     list_filter = ["room_type__accommodation__name"]
     search_fields = ["name", "room_type__name", "room_type__accommodation__name"]
 
 
 @admin.register(PackageDailyAvailability)
 class PackageDailyAvailabilityAdmin(admin.ModelAdmin):
-    list_display = ["__str__", "price"]
+    list_display = ["__str__", "retail_price", "cost_price"]
     search_fields = ["package"]
