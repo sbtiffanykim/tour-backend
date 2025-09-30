4c96312ed1d25faa0d69c1761ea8dbc43a03c6ff
Tiffany Subin Kim <sbtiffanykim@gmail.com>
2025-09-30 14:08:02 +0900

    feat(bookings): add booking urls

--- config/urls.py
+++ config/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("api/v1/users/", include("users.urls")),
     path("api/v1/accommodations/", include("accommodations.urls")),
     path("api/v1/wishlists/", include("wishlists.urls")),
+    path("api/v1/bookings/", include("bookings.urls")),
 ]
