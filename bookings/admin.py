730d7e460e9d3b289e2b15f748bc42c68b93aa77
Tiffany Subin Kim <sbtiffanykim@gmail.com>
2025-09-30 12:33:06 +0900

    feat(bookings): add admin

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Booking
 
-# Register your models here.
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ["user_or_guest", "package", "check_in", "check_out", "status"]
+    list_filter = ["status"]
+
+    def user_or_guest(self, obj):
+        if obj.guest_user:
+            return obj.guest_user.first_name
+        return obj.user.username
+
+    user_or_guest.short_description = "User"
