fcf04ff6826569bb13b6b914e46dc22686ccc5d8
Tiffany Subin Kim <sbtiffanykim@gmail.com>
2025-10-01 16:52:18 +0900

    feat(accommodations): add an url related to room types

--- accommodations/urls.py
+++ accommodations/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
+from room_types.views import RoomTypeCollectionView
 from packages.views import RoomPackageListView, AvailableRoomPackagesView
 
 urlpatterns = [
     path("", views.AccommodationCollectionView.as_view()),  # GET, POST
     path("<int:pk>", views.AccommodationDetailView.as_view()),
+    path("<int:pk>/room-types", RoomTypeCollectionView.as_view()),
     path("<int:pk>/room-packages", RoomPackageListView.as_view()),
     path("<int:pk>/available-room-packages", AvailableRoomPackagesView.as_view()),
     path("amenities", views.AmenityCollectionView.as_view()),  # GET, POST
